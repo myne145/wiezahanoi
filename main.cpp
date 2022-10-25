5ed51352c45cc1361b439cc4c0b76f750e1d9780
myne145 <antoni.kucz@gmail.com>
2022-10-25 18:00:04 +0200

    25oct

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #define ELEMENTS 6
+#define POLES 3
 
 using namespace std;
 
@@ -9,18 +10,40 @@ vector <int> poleTwo = {-1,-1,-1,-1,-1,-1};
 vector <int> poleThree = {-1,-1,-1,-1,-1,-1};
 
 void showArrays() {
+    bool isEmpty = false;
     string line = "  ";
     for(int i = ELEMENTS - 1; i >= 0; i--) {
         if(poleOne[i] != -1)
-            line = line + to_string(poleOne[i]) + "  |";
-        if(poleTwo[i] != -1)
-            line = line + to_string(poleTwo[i]) + "  |";
-        if(poleThree[i] != -1)
-            line = line + to_string(poleThree[i]) + "  |";
+            line = line + to_string(poleOne[i]) + "  |  ";
+        if(poleTwo[i] != -1) {
+            line = line + to_string(poleTwo[i]) + "  |  ";
+
+        }
+        if(poleThree[i] != -1) {
+
+            for(int j = 0; j < ELEMENTS - 1; j++) {
+                if(poleTwo[j] != -1) {
+                    isEmpty = true;
+                }
+            }
+            if(!isEmpty) {
+                line = line + "   |  " + to_string(poleThree[i]) + "  |  ";
+                isEmpty = false;
+            }
+            if(isEmpty)
+                line = line + to_string(poleThree[i]) + "  |  ";
+        }
         cout << line << endl;
+
         line = "  ";
     }
 }
+
+//void checkIfEmpty(vector <int> pole) {
+////        for(int i = 0; i < pole.size(); i++) {
+////            pole.at(i) = "  |  ";
+//    }
+//}
 
 void moveToSlot(int slotNumber, int slotToMove) {
     int temp;
@@ -28,29 +51,43 @@ void moveToSlot(int slotNumber, int slotToMove) {
         case 1:
             temp = poleOne.back();
             poleOne.back() = -1;
+            break;
         case 2:
             temp = poleTwo.back();
             poleTwo.back() = -1;
+            break;
         case 3:
             temp = poleThree.back();
             poleThree.back() = -1;
+            break;
     }
+    int x = 5;
     switch(slotToMove) {
         case 1:
-            poleOne.erase(poleOne.begin());
-            poleOne.insert(poleOne.begin(), temp);
+            poleOne.erase(*x);
+            poleOne.insert(*x, temp);
+            break;
         case 2:
             poleTwo.erase(poleTwo.begin());
             poleTwo.insert(poleTwo.begin(), temp);
+            break;
         case 3:
+            //checkIfEmpty(poleTwo);
             poleThree.erase(poleThree.begin());
             poleThree.insert(poleThree.begin(), temp);
+            break;
     }
 }
 int main() {
-
-    moveToSlot(1,2);
+    int whichSlot, whereToPut;
     showArrays();
+
+    do {
+        cin >> whichSlot;
+        cin >> whereToPut;
+        moveToSlot(whichSlot, whereToPut);
+        showArrays();
+    } while(whichSlot != -1 || whereToPut != -1);
 
     return 0;
 }
